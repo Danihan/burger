fccb0e7de95b535c8342d040a13fa7b94018055a
Dan Linehan <wagist@gmail.com>
2018-10-24 12:28:39 +0200

    Fix burger model

--- models/burger.js
+++ models/burger.js
@@ -4,12 +4,12 @@ async function selectAll () {
   return orm.selectAll('burgers')
 }
 
-async function insertOne ({ name, devoured }) {
-  return orm.insertOne('burgers', { name, devoured })
+async function insertOne (data) {
+  return orm.insertOne('burgers', data)
 }
 
-async function updateOne (id, { name, devoured }) {
-  return orm.updateOne('burgers', id, { name, devoured })
+async function updateOne (id, data) {
+  return orm.updateOne('burgers', id, data)
 }
 
 module.exports = {
