c5635ce40adc537f7964c75956a2585a4c8befa7
Dan Linehan <wagist@gmail.com>
2018-10-30 14:19:41 +0100

    Use environment variables for database connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
+  host: process.env.DATABASE_HOST || 'localhost',
+  user: process.env.DATABASE_USER || 'root',
+  password: process.env.DATABASE_PASSWORD || 'root',
+  database: process.env.DATABASE_NAME || 'burgers_db'
 })
 
 connection.connect()
